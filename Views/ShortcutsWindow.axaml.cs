0760bf63af89348467399ead0b17498f8660cbac
Simone Fortino <simonefortino41@gmail.com>
2026-08-15 17:59:48 +0200

    added top menu, new shortcuts.

---
+++ Views/ShortcutsWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SharpCalculator.Views;
+
+public partial class ShortcutsWindow : Window
+{
+    public ShortcutsWindow()
+    {
+        InitializeComponent();
+    }
+}

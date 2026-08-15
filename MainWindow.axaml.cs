a6e443b857957e778ff81b5b37abb5404f1729cb
Simone Fortino <simonefortino41@gmail.com>
2026-08-15 11:26:56 +0200

    added support for keyborard keys; new shortcut for changing themes; slightly modified light theme palette.

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,5 +1,8 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Styling;
 
 namespace SharpCalculator;
 
@@ -45,4 +48,71 @@ public partial class MainWindow : Window
     {
         MainTextBox.Text = _calculator.Clear();
     }
+
+    private void OnKeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.KeyModifiers.HasFlag(KeyModifiers.Control))
+        {
+            if(e.Key is Key.T)
+                ToggleTheme();
+                
+        }
+        
+        var (val, type) = e.Key switch
+        {
+            // Numeri (Tastiera standard e Tastierino)
+            Key.D0 or Key.NumPad0 => ("0", "NUMBER"),
+            Key.D1 or Key.NumPad1 => ("1", "NUMBER"),
+            Key.D2 or Key.NumPad2 => ("2", "NUMBER"),
+            Key.D3 or Key.NumPad3 => ("3", "NUMBER"),
+            Key.D4 or Key.NumPad4 => ("4", "NUMBER"),
+            Key.D5 or Key.NumPad5 => ("5", "NUMBER"),
+            Key.D6 or Key.NumPad6 => ("6", "NUMBER"),
+            Key.D7 or Key.NumPad7 => ("7", "NUMBER"),
+            Key.D8 or Key.NumPad8 => ("8", "NUMBER"),
+            Key.D9 or Key.NumPad9 => ("9", "NUMBER"),
+
+            // Operatori
+            Key.Add => ("+", "OPERATOR"),
+            Key.Subtract or Key.OemMinus => ("-", "OPERATOR"),
+            Key.Multiply => ("*", "OPERATOR"),
+            Key.Divide => ("/", "OPERATOR"),
+
+            // Azioni speciali
+            Key.Enter or Key.Return => ("=", "EQUALS"),
+            Key.Back => ("BACKSPACE", "CLEAR"),
+            Key.Escape => ("ESCAPE", "CLEAR"),
+
+            // Tasto non gestito
+            _ => (string.Empty, "UNKNOWN")
+        };
+
+        switch (type)
+        {
+            case "NUMBER":
+                MainTextBox.Text = _calculator.PressNumber(MainTextBox.Text ?? "0", val);
+                break;
+            case "OPERATOR":
+                MainTextBox.Text = _calculator.PressOperator(MainTextBox.Text ?? "0", val);
+                break;
+            case "EQUALS":
+                MainTextBox.Text = _calculator.PressEquals(MainTextBox.Text ?? "0");
+                break;
+            case "CLEAR":
+                MainTextBox.Text = _calculator.Clear();
+                break;
+        }
+        
+    }
+
+    private void ToggleTheme()
+    {
+        var app = Application.Current;
+        if (app is null) return;
+        
+        if(app.ActualThemeVariant == ThemeVariant.Dark)
+            app.RequestedThemeVariant =  ThemeVariant.Light;
+        else if (app.ActualThemeVariant == ThemeVariant.Light)
+            app.RequestedThemeVariant = ThemeVariant.Dark;
+    }   
 }
